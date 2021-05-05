99181f4a86d4848e35ab4f1c95b5bcd9e995bd38
tigranb2 <59093401+tigranb2@users.noreply.github.com>
2021-05-05 10:41:19 -0400

    removed loop

--- run.sh
+++ run.sh
@@ -2,8 +2,5 @@ if [[ -z $1 ]]; then
   echo "Please specify the geth write frequency"
 else
   chmod 700 simulateWrites
-    python3 start.py $1
-    python3 analysis.py
-  done
-  cat anaylsis.txt
+  python3 start.py $1
 fi
