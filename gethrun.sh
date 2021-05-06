c9ca3ff976db7872b3f71a4373b8d759df5b50cf
tigranb2 <59093401+tigranb2@users.noreply.github.com>
2021-05-06 19:07:08 -0400

    added geth initialization

---
+++ gethrun.sh
@@ -0,0 +1,2 @@
+geth --datadir geth init genesis.json
+cp keys/UTC--2021-05-06T22-54-41.813982768Z--af193f783d7b2c4aff8a929eaad33767738859b6 /geth/keystore
--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 if [[ -z $1 ]]; then
   echo "Please specify the geth write frequency"
 else
+  chmod 700 cassrun.sh
+  chmod 700 gethrun.sh
   chmod 700 simulateWrites
+  . gethrun.sh
   python3 start.py $1 $2
+  rm -r geth
   killall java
 fi
