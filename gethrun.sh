d88923ddc84548b19d96eab9d6a091bd8b6d1747
tigranb2 <59093401+tigranb2@users.noreply.github.com>
2021-05-06 23:24:23 -0400

    adjusted key directory

--- gethrun.sh
+++ gethrun.sh
@@ -1,3 +1,3 @@
 mkdir -p /geth/keystore
 geth --datadir geth init genesis.json
-sudo cp /keys/UTC--2021-05-07T03-16-23.709229300Z--1feb3ff7be9be6a6182e6ece317a043a4f0337ab /geth/keystore
+cp ./keys/UTC--2021-05-07T03-16-23.709229300Z--1feb3ff7be9be6a6182e6ece317a043a4f0337ab /geth/keystore
