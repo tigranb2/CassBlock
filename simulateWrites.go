3d062d165d0c7b04c9ffc5d34df0d34dc40b8bf3
tigranb2 <59093401+tigranb2@users.noreply.github.com>
2021-05-13 10:49:03 -0400

    updated geth startup

--- simulateWrites.go
+++ simulateWrites.go
@@ -3,18 +3,18 @@ package main
 import (
 	"CassBlock/message"
 	"bytes"
+	"crypto/sha256"
 	"encoding/gob"
 	"fmt"
+	"github.com/gocql/gocql"
+	"math/rand"
 	"os"
 	"os/exec"
-	"time"
-	"math/rand"
 	"strconv"
-	"crypto/sha256"
-	"github.com/gocql/gocql"
+	"time"
 )
 
-/* 
+/*
 CREATE KEYSPACE test_keyspace WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};
 
 CREATE TABLE test_sensor (
@@ -27,51 +27,52 @@ CREATE TABLE test_sensor (
 */
 
 var Session *gocql.Session
+var ip string
 
 func main() {
 	arguments := os.Args
-	if len(arguments) < 2 {
-		fmt.Println("Please specify row count...")
+	if len(arguments) < 3 {
+		fmt.Println("Please specify sensor id, row count...")
 		return
 	}
-	rowCount, _ := strconv.Atoi(arguments[1]) //numbers of rows per sensor
+	id, _ := strconv.Atoi(arguments[1])       //numbers of rows per sensor
+	rowCount, _ := strconv.Atoi(arguments[2]) //numbers of rows per sensor
+	ip = fmt.Sprintf("10.0.0.%v", id)
 
 	cassandraInit("127.0.0.1")
-	simulateWrites(rowCount)
+	simulateWrites(id, rowCount)
 }
 
-func cassandraInit(CONNECT string){
+func cassandraInit(CONNECT string) {
 	var err error
 	cluster := gocql.NewCluster(CONNECT) //connect to cassandra database
 	cluster.Keyspace = "test_keyspace"
-	Session, err = cluster.CreateSession() 
+	Session, err = cluster.CreateSession()
 	if err != nil {
 		fmt.Print(err)
 	}
 }
 
-func simulateWrites(rowCount int) {
+func simulateWrites(id, rowCount int) {
 	var cassLatencies, gethLatencies []message.Latencies
-	s := []message.Test_sensor{}
+	var s []message.Test_sensor
 	write, row, randI := 0, 0, 0
 	fmt.Println("Generating data...")
 	for {
 		row++
 		write++
-		randI = rand.Intn(3000 - 1000) + 1000
-		for id := 1; id <= 5; id++ {
-			str := strconv.Itoa(randI/1000) //returns string of random int
-			cassWR := message.Test_sensor{Sensor_id: id, Row: row, Temperature: (str+"km"), Speed: (str+"km"), Latencies: message.Latencies{}} //stores info for cassandra write & read 
-			cassandraTest(&cassWR)
-			fmt.Printf("Sensor: %v, Row: %v, write latency: %vms, read latency: %vms\n", id, row, cassWR.Latencies.WriteLatency, cassWR.Latencies.ReadLatency)
-			cassLatencies = append(cassLatencies, cassWR.Latencies)
-			s = append(s, cassWR)
-		}
+		randI = rand.Intn(3000-1000) + 1000
+		str := strconv.Itoa(randI / 1000)                                                                                                  //returns string of random int
+		cassWR := message.Test_sensor{Sensor_id: id, Row: row, Temperature: str + "km", Speed: str + "km", Latencies: message.Latencies{}} //stores info for cassandra write & read
+		cassandraTest(&cassWR)
+		fmt.Printf("Sensor: %v, Row: %v, write latency: %vms, read latency: %vms\n", 1, row, cassWR.Latencies.WriteLatency, cassWR.Latencies.ReadLatency)
+		cassLatencies = append(cassLatencies, cassWR.Latencies)
+		s = append(s, cassWR)
 
-		if len(s) % rowCount == 0 {
+		if len(s)%rowCount == 0 {
 			metadata := hash(s)
 			gethWR := message.Latencies{} //stores info for geth write & read
-			gethTest("ws://10.0.0.1:8101", metadata, &gethWR)
+			gethTest("ws://"+ip+"8101", metadata, &gethWR)
 			gethLatencies = append(gethLatencies, gethWR)
 			fmt.Printf("Go-Ethereum - write latency: %vms, read latency: %vms\n", gethWR.WriteLatency, gethWR.ReadLatency)
 			s = []message.Test_sensor{}
@@ -81,7 +82,7 @@ func simulateWrites(rowCount int) {
 		if write == 100 {
 			break
 		}
-		time.Sleep(time.Duration(randI)*time.Millisecond) //sleeps for 1 - 3 seconds
+		time.Sleep(time.Duration(randI) * time.Millisecond) //sleeps for 1 - 3 seconds
 	}
 
 	cassWriteLatency, cassReadLatency := average(cassLatencies)
@@ -91,7 +92,7 @@ func simulateWrites(rowCount int) {
 }
 
 func cassandraTest(data *message.Test_sensor) {
-	start := time.Now().UnixNano()/1000000
+	start := time.Now().UnixNano() / 1000000
 	//create new row in test_table
 	if err := Session.Query("INSERT INTO test_sensor(sensor_id,row,temperature,speed) VALUES(?, ?, ?, ?)", data.Sensor_id, data.Row, data.Temperature, data.Speed).Exec(); err != nil {
 		fmt.Println(err)
@@ -99,20 +100,20 @@ func cassandraTest(data *message.Test_sensor) {
 
 	data.Latencies.WriteLatency = int(time.Now().UnixNano()/1000000 - start)
 
-	start = time.Now().UnixNano()/1000000
+	start = time.Now().UnixNano() / 1000000
 	//read new row in test_table
 	if err := Session.Query(`SELECT speed FROM test_sensor WHERE sensor_id = ? AND row = ?`, data.Sensor_id, data.Row).Exec(); err != nil {
 		fmt.Println(err)
-	}	
+	}
 
 	data.Latencies.ReadLatency = int(time.Now().UnixNano()/1000000 - start)
 }
 
 func gethTest(connect string, msg [32]byte, gethWR *message.Latencies) {
-	start := time.Now().UnixNano()/1000000
+	start := time.Now().UnixNano() / 1000000
 	//writes data into geth transaction
 	tx := fmt.Sprintf("eth.sendTransaction({from:eth.accounts[0],to:eth.accounts[0],value:1,data:web3.toHex('%v')})", msg)
-	output, err := exec.Command("geth", "attach", connect, "--exec", tx).CombinedOutput() 
+	output, err := exec.Command("geth", "attach", connect, "--exec", tx).CombinedOutput()
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -120,11 +121,11 @@ func gethTest(connect string, msg [32]byte, gethWR *message.Latencies) {
 
 	transactionID := string(output)
 	fmt.Println(transactionID)
-	gethWR.WriteLatency =  int(time.Now().UnixNano()/1000000 - start)
+	gethWR.WriteLatency = int(time.Now().UnixNano()/1000000 - start)
 
-	start = time.Now().UnixNano()/1000000
+	start = time.Now().UnixNano() / 1000000
 	tx = fmt.Sprintf("eth.getTransaction(%v)", transactionID)
-	exec.Command("geth", "attach", connect, "--exec", tx).Run() 
+	exec.Command("geth", "attach", connect, "--exec", tx).Run()
 
 	gethWR.ReadLatency = int(time.Now().UnixNano()/1000000 - start)
 }
