e436e9f32633bad10ef1a23e1929ab50b7d53792
tigranb2 <59093401+tigranb2@users.noreply.github.com>
2021-05-13 11:39:56 -0400

    fixed typo

--- geth.py
+++ geth.py
@@ -21,7 +21,7 @@ miner_start = (
 node_start = (
     f"nohup geth --nodiscover --ipcdisable --networkid {network_id} --port {port} "
     "--syncmode 'full' --datadir=$DDR{n} --bootnodes $ENODE_ADDRESS "
-    "--ws --wsaddr 10.0.0.1 --wsport 8101 --rpcapi eth,web3,personal,net,admin,miner "
+    "--ws --wsaddr 10.0.0.{n} --wsport 8101 --rpcapi eth,web3,personal,net,admin,miner "
     f"--gasprice '1' --mine --minerthreads {miner_thread} "
     "--etherbase='0x1feb3ff7be9be6a6182e6ece317a043a4f0337ab' "
     "--unlock '0x1feb3ff7be9be6a6182e6ece317a043a4f0337ab' -allow-insecure-unlock --password ./password.sec &"
