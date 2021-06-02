e1d6958c281825c2a3dadc4b9b98fdefb6240567
tigranb2 <59093401+tigranb2@users.noreply.github.com>
2021-06-01 23:48:12 -0400

    Delete cassandra.py

--- cassandra.py
+++
@@ -1,4 +0,0 @@
-init_keyspace = ('''~/cassandra/bin/cqlsh -e "CREATE KEYSPACE test_keyspace WITH replication '''
-                 '''= {'class': 'SimpleStrategy', 'replication_factor': '1'};" 127.0.0.1''')
-init_table = ('~/cassandra/bin/cqlsh -e "USE test_keyspace; CREATE TABLE test_sensor '
-              'sensor_id int,row int,temperature text,speed text,PRIMARY KEY ((sensor_id), row));" 127.0.0.1')
