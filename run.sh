adced63e2c0f4ea6f9800890e8a3b78d56e31f0b
tigranb2 <59093401+tigranb2@users.noreply.github.com>
2021-06-03 17:04:41 -0400

    added call to analysis.py

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-if [[ -z $2 ]]; then
-  echo "Please specify node count and row count..."
+if [[ -z $3 ]]; then
+  echo "Please specify node count, row count, and a test description..."
 else
   rm -r ~/.ccm
   rm avg-latencies.txt
@@ -9,6 +9,8 @@ else
   chmod 700 simulateWrites
   . gethrun.sh $1
   python3 start.py $1 $2
+  python3 analysis.py $3
+  cat $3-data.txt
   rm -r ~/go/src/CassBlock/blockchain/
   killall java
   killall geth
