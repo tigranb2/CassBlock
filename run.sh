69524a5b98d9e98d2004e3cd0a1728829ce3d4c7
tigranb2 <59093401+tigranb2@users.noreply.github.com>
2021-06-02 22:44:18 -0400

    reordered commands

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 if [[ -z $2 ]]; then
   echo "Please specify node count and row count..."
 else
-  chmod 700 gethrun.sh
-  chmod 700 simulateWrites
   rm -r ~/.ccm
   ccm create test -v 3.0.24
   go build ./simulateWrites.go
+  chmod 700 gethrun.sh
+  chmod 700 simulateWrites
   . gethrun.sh $1
   python3 start.py $1 $2
   rm -r ~/go/src/CassBlock/blockchain/
