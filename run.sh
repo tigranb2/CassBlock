3f768710889190825c407946fc673b41a07a7cd0
tigranb2 <59093401+tigranb2@users.noreply.github.com>
2021-06-03 11:11:44 -0400

    removes latency file between runs

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ if [[ -z $2 ]]; then
   echo "Please specify node count and row count..."
 else
   rm -r ~/.ccm
+  rm avg-latencies.txt
   ccm create test -v 3.0.24
   go build ./simulateWrites.go
   chmod 700 gethrun.sh
