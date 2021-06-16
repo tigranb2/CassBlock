908e352e625dfabb8f0516de07ec4a5bb0263eba
tigranb2 <59093401+tigranb2@users.noreply.github.com>
2021-06-16 19:04:10 -0400

    changed read

--- message/message.go
+++ message/message.go
@@ -1,12 +1,6 @@
 package message
 
-type Latencies struct {
-	WriteLatency, ReadLatency int
-	
-}
-
 type Test_sensor struct {
-	Sensor_id, Row int
-	Temperature, Speed string
-	Latencies Latencies
+	Sensor_id, Row, Writeset int
+	Speed float64
 }
