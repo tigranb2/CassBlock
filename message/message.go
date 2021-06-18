6d8c20dce146e1ca0e406c543a98f102e01d2097
tigranb2 <59093401+tigranb2@users.noreply.github.com>
2021-06-18 16:04:01 -0400

    added struct for geth data

--- message/message.go
+++ message/message.go
@@ -2,11 +2,11 @@ package message
 
 type Test_sensor struct {
 	Sensor_id, Row, Writeset int
-	Speed float64
+	Speed                    float64
 }
 
 type BlockchainData struct {
-	SensorId int
-	Min, Max            float64
+	SensorId  int
+	Min, Max  float64
 	Timestamp int64
 }
